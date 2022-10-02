3fd091933358694c022fb411dfde04e57fdf07eb
eveporcello <eve@moonhighway.com>
2022-10-02 16:21:50 -0700

    Adds pet gateway details

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const { ApolloServer } = require("apollo-server");
-const { ApolloGateway } = require("@apollo/gateway");
+const {
+  ApolloGateway,
+  IntrospectAndCompose,
+} = require("@apollo/gateway");
 
-const gateway = new ApolloGateway();
+const gateway = new ApolloGateway({
+  supergraphSdl: new IntrospectAndCompose({
+    subgraphs: [
+      {
+        name: "pets",
+        url: "https://federated-mock-pets.herokuapp.com",
+      },
+      {
+        name: "customers",
+        url: "https://federated-mock-customers.herokuapp.com",
+      },
+    ],
+  }),
+});
 
 const start = async () => {
   const server = new ApolloServer({
     gateway,
-    subscriptions: false
   });
   server.listen(process.env.PORT).then(({ url }) => {
     console.log(
